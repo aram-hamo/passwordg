64be1392a41f99280e2e8ce8b84b8ab42f10d252
Aram Hamo <aramhamo@protonmail.com>
2021-01-02 18:16:55 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="UTF-8">
+        <meta name=theme-color content="#007BFF">
         <link href=/bootstrap.css rel=stylesheet></link>
         <style>
         body{
@@ -18,22 +19,23 @@
 	</head>
 	<body>
     <form method=post>
-    <input name="capitalize" type="radio" value=capitalize default></input>
-    <label class=silver > Include Capitalize chars</label>
+    <input name="capitalize" type="radio" id=capitalize value=capitalize default></input>
+    <label for=capitalize class=silver > Include Capitalize chars</label>
     <br>
-    <input name="capitalize" type="radio" value=no-capitalize></input>
-    <label class=silver > No Capitalize</label>
+    <input name="capitalize" type="radio" id=no-capitalize value=no-capitalize></input>
+    <label class=silver for=no-capitalize > No Capitalize</label>
     <hr>
-    <input name="numerals" type="radio" value=numerals></input>
-    <label class=silver > Include at least one number</label>
+    <input name="numerals" id=numerals type="radio" value=numerals></input>
+    <label class=silver for=numerals > Include at least one number</label>
     <br>
-    <input name="numerals" type="radio" value=no-numerals></input>
-    <label class=silver > Do not include any number</label>
+    <input name="numerals" id=no-numerals type="radio" value=no-numerals></input>
+    <label class=silver for=no-numerals> Do not include any number</label>
     <hr>
-    <input name="specialS" type="checkbox" value=specialS></input>
-    <label class=silver > include symblos</label>
+    <input name="specialS" id=specialS type="checkbox" value=specialS></input>
+    <label class=silver for=specialS> include symblos</label>
     <hr>
-    <input name="generate" class="btn-primary btn submitB" value="Generate" type="submit"></input>
+    <input name="generate" class="btn-primary btn submitB" value="Generate"
+    type="submit"></input>
     <br>
     <br>
     </div> 
@@ -56,6 +58,7 @@ if(isset($_POST['generate'])){
     }
     echo '<div>' . shell_exec($command) . '</div>';
 }
+    echo '<br><br><br><br><br><br>';
 ?>
 	</body>
 </html>
